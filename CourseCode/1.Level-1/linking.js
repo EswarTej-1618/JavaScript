91a3ef13f728274e1f9c70d5b43fa65e303cfea3
EswarTej-1618 <eswarpavanteja@gmail.com>
2024-10-18 16:44:47 +0530

    Adding JavaScript

---
+++ CourseCode/1.Level-1/linking.js
@@ -0,0 +1,9 @@
+console.log('I am  from linking.js');
+// this method is used to clear the console
+console.clear();
+
+ /*this is the multiline comment
+ in the javascript and 
+ so called Css to!
+*/
+console.log('This is the message after clearing the log!....')
